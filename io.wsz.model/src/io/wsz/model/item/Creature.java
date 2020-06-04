4936d80544e68d49e2f22d47a8ab3534f6529eda
wsz82 <wojteksz82@gmail.com>
2020-06-05 00:12:11 +0200

    walking between locations

--- io.wsz.model/src/io/wsz/model/item/Creature.java
+++ io.wsz.model/src/io/wsz/model/item/Creature.java
@@ -2,7 +2,12 @@ package io.wsz.model.item;
 
 import io.wsz.model.Controller;
 import io.wsz.model.content.Content;
+import io.wsz.model.layer.Layer;
+import io.wsz.model.location.Location;
 import io.wsz.model.stage.Coords;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Creature extends PosItem {
     private Coords dest;
@@ -24,13 +29,18 @@ public class Creature extends PosItem {
     }
 
     public void move() {
+        ItemType[] types = new ItemType[] {ItemType.OBSTACLE, ItemType.TELEPORT};
+        Content c = getCornersContent(types);
+        if (c != null) {
+            PosItem item = c.getItem();
+            ItemType type = item.getType();
+            switch (type) {
+                case OBSTACLE -> escapeObstacle(c);
+                case TELEPORT -> enterTeleport((Teleport) item);
+            }
+        }
         if (dest == null) {
             return;
-        }
-        Content c = getObstacle();
-        if (c != null) {
-            Coords free = Controller.get().getBoard().getFreePos(getCorners(), c);
-            setDest(calcDest(free));
         }
         double x1 = pos.getX();
         double x2 = dest.getX();
@@ -50,9 +60,13 @@ public class Creature extends PosItem {
         pos.setY(y3);
     }
 
-    private Content getObstacle() {
+    private void escapeObstacle(Content c) {
+        Coords free = Controller.get().getBoard().getFreePos(getCorners(), c);
+        setDest(calcDest(free));
+    }
+
+    private Content getCornersContent(ItemType[] types) {
         Coords[] poss = getCorners();
-        ItemType[] types = new ItemType[] {ItemType.OBSTACLE};
         return Controller.get().getBoard().lookForContent(poss, types, false);
     }
 
@@ -114,7 +128,50 @@ public class Creature extends PosItem {
         ItemType type = item.getType();
         switch (type) {
             case CREATURE -> interactWithCreature((Creature) item);
+            case OBSTACLE -> doNothing();
             default -> setDest(calcDest(pos));
+        }
+    }
+
+    private void doNothing() {
+    }
+
+    private void enterTeleport(Teleport t) {
+        List<Location> singleLocation = Controller.get().getLocationsList().stream()
+                .filter(l -> l.getName().equals(t.getLocationName()))
+                .collect(Collectors.toList());
+        Location target = singleLocation.get(0);
+        if (target == null) {
+            return;
+        }
+        int targetLevel = t.getExitLevel();
+        List<Layer> singleLayer = target.getLayers().get().stream()
+                .filter(l -> l.getLevel() == targetLevel)
+                .collect(Collectors.toList());
+        Layer targetLayer = singleLayer.get(0);
+        if (targetLayer == null) {
+            return;
+        }
+        Coords targetPos = t.getExit();
+        double targetX = targetPos.getX();
+        int targetWidth = target.getWidth();
+        double targetY = targetPos.getY();
+        int targetHeight = target.getHeight();
+        if (targetX < targetWidth && targetY < targetHeight) {
+            List<Content> current = Controller.get().getCurrentLocation().getContent();
+            List<Content> singleContent = current.stream()
+                    .filter(c -> c.getItem().equals(this))
+                    .collect(Collectors.toList());
+            Content thisContent = singleContent.get(0);
+            current.remove(thisContent);
+            target.getContents().get().add(thisContent);
+
+            Controller.get().getCurrentLocation().setLocation(target);
+            Controller.get().getCurrentLayer().setLayer(targetLayer);
+            pos.setX(targetX);
+            pos.setY(targetY);
+            level = targetLevel;
+            dest = null;
         }
     }
 
